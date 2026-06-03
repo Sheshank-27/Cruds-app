bde70e926068f9c5e9f283b83d40797d9a72fc43
sheshank-27 <sheshanklakhi16@gmail.com>
2026-06-03 19:33:04 +0530

    ci cd test 2

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def home():
     connection.close()
 
     html = """
-    <h1>Employee Management System -ci-cd-test</h1>
+    <h1>Employee Management System -ci-cd-test2</h1>
 
     <form action='/add' method='post'>
         Name: <input type='text' name='name'><br><br>
